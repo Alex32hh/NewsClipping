05ca1d107c4bca486b5a9f0485ecb4d0f54eb399
Alexandre <55396327+Alex32hh@users.noreply.github.com>
2020-08-02 19:40:29 +0100

    resalvo de rotina

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, { Component, useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Link, useLocation, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
 import Home from './screens/home';
-import Scrollable from './screens/scrollabs';
-
 import results from './screens/results';
-import axios from 'axios';
 import './App.css';
 import './mainpage.css';
 import instagram from './images/instagram.png';
@@ -20,7 +17,7 @@ import print from './images/print.png';
 import plusSignal from './images/plus2.png';
 import settings from './images/gear2.png';
 import share from './images/share.png';
-import Popup from "reactjs-popup";
+
 import { Button, Alert } from 'react-bootstrap';
 export default function App() {
 
@@ -32,107 +29,106 @@ export default function App() {
       <div className="App">
         {path == "/" ? Home() :
           <>
-          <header>
-          <div className='topBar'>
-            <div className="barcontent">
-              <div className='leftside'>
-                <Link> <item>Contactos</item></Link>
-                <Link> <item>Contactos</item></Link>
-                <Link> <item>Contactos</item></Link>
-                <Link> <item>Contactos</item></Link>
-              </div>
-
-              <div className='rightside'>
-                <Link to="1"> <item><img src={whatsapp} /></item> </Link>
-                <Link to="2"> <item><img src={youtube} /></item>  </Link>
-                <Link to="3"> <item><img src={facebook} /></item> </Link>
-                <Link to="4"> <item><img src={instagram} /></item></Link>
-              </div>
-
-            </div>
-
-          </div>
-
-          <nav>
-            <div className='divide'>
-              <img src={logo} className='image-logo' />
-            </div>
-
-            <div className='divide'>
-
-              <div className='center-box'>
-
-                <div className='center-top'>
-
-                  <div></div>
-
-                  <div>
-                    <button>
-                      <img src={search} />
-                    </button>
+            <header>
+              <div className='topBar'>
+                <div className="barcontent">
+                  <div className='leftside'>
+                    <Link> <item>Contactos</item></Link>
+                    <Link> <item>Contactos</item></Link>
+                    <Link> <item>Contactos</item></Link>
+                    <Link> <item>Contactos</item></Link>
                   </div>
 
-                  <div>
-                    <input type='text' placeholder="Pesquisar..." />
+                  <div className='rightside'>
+                    <Link to="1"> <item><img src={whatsapp} /></item> </Link>
+                    <Link to="2"> <item><img src={youtube} /></item>  </Link>
+                    <Link to="3"> <item><img src={facebook} /></item> </Link>
+                    <Link to="4"> <item><img src={instagram} /></item></Link>
                   </div>
 
-                  <div></div>
-                </div>
-
-                <div className='center-bottom'>
-                  <div></div>
-
-                  <div className='catego-items'>
-                    <Link style={{ color: 'inherit', textDecoration: 'inherit' }}><div className='link-hover-manifexto'> <img src={plusSignal} /> Criar Alerta</div></Link>
-                    <Link style={{ color: 'inherit', textDecoration: 'inherit' }}><div className='link-hover-manifexto'><img src={settings} /> Opções</div></Link>
-                    <Link style={{ color: 'inherit', textDecoration: 'inherit' }}><div className='link-hover-manifexto'><img src={share} /> Partilhar</div></Link>
-                    <Link style={{ color: 'inherit', textDecoration: 'inherit' }}><div className='link-hover-manifexto'><img src={print} /> Imprimir</div></Link>
-                  </div>
-
-                  <div></div>
                 </div>
 
               </div>
 
-            </div>
-
-            <div className='divide'>
-              <div className='images-items'>
-
-
-                <div className='btn-alertas'>
-                  <Link to='/alertas'>
-                    <img src={Sino} />
-                    <label>Alertas</label>
-                  </Link>
+              <nav>
+                <div className='divide'>
+                  <img src={logo} className='image-logo' />
                 </div>
 
+                <div className='divide'>
 
-                <div className='btn-conta'>
-                  <Link to='/perfil'>
-                    <img src={user} />
-                    <label>Perfil</label>
-                  </Link>
+                  <div className='center-box'>
+
+                    <div className='center-top'>
+
+                      <div></div>
+
+                      <div>
+                        <button>
+                          <img src={search} />
+                        </button>
+                      </div>
+
+                      <div>
+                        <input type='text' placeholder="Pesquisar..." />
+                      </div>
+
+                      <div></div>
+                    </div>
+
+                    <div className='center-bottom'>
+                      <div></div>
+
+                      <div className='catego-items'>
+                        <Link style={{ color: 'inherit', textDecoration: 'inherit' }}><div className='link-hover-manifexto'> <img src={plusSignal} /> Criar Alerta</div></Link>
+                        <Link style={{ color: 'inherit', textDecoration: 'inherit' }}><div className='link-hover-manifexto'><img src={settings} /> Opções</div></Link>
+                        <Link style={{ color: 'inherit', textDecoration: 'inherit' }}><div className='link-hover-manifexto'><img src={share} /> Partilhar</div></Link>
+                        <Link style={{ color: 'inherit', textDecoration: 'inherit' }}><div className='link-hover-manifexto'><img src={print} /> Imprimir</div></Link>
+                      </div>
+
+                      <div></div>
+                    </div>
+
+                  </div>
+
                 </div>
 
+                <div className='divide'>
+                  <div className='images-items'>
 
-              </div>
+
+                    <div className='btn-alertas'>
+                      <Link to='/alertas'>
+                        <img src={Sino} />
+                        <label>Alertas</label>
+                      </Link>
+                    </div>
+
+
+                    <div className='btn-conta'>
+                      <Link to='/perfil'>
+                        <img src={user} />
+                        <label>Perfil</label>
+                      </Link>
+                    </div>
+
+
+                  </div>
+                </div>
+              </nav>
+            </header>
+
+            <div className='container'>
+              <Route exact path="/" component={Home} />
+              <Route path="/alertas" component={corpo} />
+              <Route path="/perfil" component={contact} />
+              <Route path="/results" component={results} />
+              <footer></footer>
+
             </div>
-          </nav>
-          </header>
-          
-          <div className='container'>
-            <Route exact path="/" component={Home} />
-            <Route path="/alertas" component={corpo} />
-            <Route path="/perfil" component={contact} />
-            <Route path="/Scrollable" component={Scrollable} />
-            <Route path="/results" component={results} />
-            <footer></footer>
-
-          </div>
           </>
-}
-     
+        }
+
       </div>
     </Router>
   );
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,23 +1,39 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link, useLocation, Redirect } from "react-router-dom";
+import React, { Component, useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
 import Sino from '../images/bell.png';
 import user from '../images/user.png';
 import logo from '../images/Logotipo_Manifexto2018.png';
 import search from '../images/search.png';
+import { useForm } from 'react-hook-form';
+
 export default function Home() {
 
-    const form = {};
+    const [redirctTo, setRedirctTo] = useState(false);
+    const { register, handleSubmit, errors } = useForm();
 
+    const onSubmit = (data) => {
+        // alert(data.p);
+        setRedirctTo(true)
+    };
 
+    // useEffect(() => {
+    //     (() => {
+    //         if (true) {
+    //             setRedirctTo(true)
+    //         }
+    //         // return to another component
+    //     })();
+    // });
 
-
-
+    if (redirctTo) {
+        return (<Redirect to="/results" />);
+    }
     return (
-    <Router>
-        <div className='main-login-Page'>
+        <Router>
+            <div className='main-login-Page'>
                 <div className='top-item'>
-                    <div></div>    
-                    
+                    <div></div>
+
                     <div>
                         <div className='alerta-icon'>
                             <Link>
@@ -32,28 +48,30 @@ export default function Home() {
                                 <label className='link-hover-manifexto'>Perfil</label>
                             </Link>
                         </div>
-                    </div>    
+                    </div>
 
                 </div>
-                
+
                 <div className='center-intem'>
                     <div>
                         <img src={logo} />
                     </div>
                     <div className='center-input-box'>
+
                         <div></div>
-                        <div>
-                            <button> <img src={search}/> </button>
-                        </div>
-                        <div>
-                            <input type="text" placeholder="Pesquisar..." />
-                        </div>
+                        <form className='form-list' onSubmit={handleSubmit(onSubmit)}>
+                            <div><button type="submit"> <img src={search} /> </button></div>
+                            <div><input type="text" placeholder="Pesquisar..." name="p" ref={register({ required: true })} /></div>
+                        </form>
                         <div></div>
+
                     </div>
 
                 </div>
-                
-        </div>
-    </Router>
+
+
+            </div>
+        </Router>
     );
+
 }
